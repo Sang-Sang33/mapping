520b67d53b9e64e569b7a3e4b9549bda1e41d2f0
luyao <luyao@mw-robot.com>
2023-06-21 17:44:19 +0800

    fix: cookie set 和 remove 的key没有与get统一

--- packages/utils/modules/cookie/index.ts
+++ packages/utils/modules/cookie/index.ts
@@ -6,8 +6,9 @@ export * from './external'
 export type TAppCookieKey = keyof typeof ECookie
 const AppCookie = {
   get: (key: TAppCookieKey) => Cookies.get(ECookie[key]),
-  set: (key: TAppCookieKey, value: string, options?: Cookies.CookieAttributes) => Cookies.set(key, value, options),
-  remove: (key: TAppCookieKey, options?: Cookies.CookieAttributes) => Cookies.remove(key, options)
+  set: (key: TAppCookieKey, value: string, options?: Cookies.CookieAttributes) =>
+    Cookies.set(ECookie[key], value, options),
+  remove: (key: TAppCookieKey, options?: Cookies.CookieAttributes) => Cookies.remove(ECookie[key], options)
 }
 
 export default AppCookie
