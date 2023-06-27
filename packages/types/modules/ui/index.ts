d24c850bd11968a1b8f529cd2a78ed0e23d4e98f
luyao <luyao@mw-robot.com>
2023-06-27 10:25:11 +0800

    feat: TLayoutRoute类型定义

--- packages/types/modules/ui/index.ts
+++ packages/types/modules/ui/index.ts
@@ -1,12 +1,20 @@
 import type { ReactNode } from 'react'
-import type { RouteObject } from 'react-router-dom'
+import type { IndexRouteObject, NonIndexRouteObject } from 'react-router-dom'
 
-export type TLayoutRouteObject = RouteObject & {
+export interface ILayoutData {
   name: string
   id: string
   icon?: ReactNode
   order?: number
-  children?: TLayoutRouteObject[]
 }
-export type TLayoutRoutes = TLayoutRouteObject[]
+
+export interface ILayoutIndexRouteObject extends ILayoutData, Omit<IndexRouteObject, 'children' | 'id'> {
+  children?: undefined
+}
+export interface ILayoutNonIndexRouteObject extends ILayoutData, Omit<NonIndexRouteObject, 'children' | 'id'> {
+  children?: ILayoutNonIndexRouteObject[]
+}
+
+export type TLayoutRouteObject = ILayoutIndexRouteObject | ILayoutNonIndexRouteObject
+export type TLayoutRoutes = ILayoutIndexRouteObject[] | ILayoutNonIndexRouteObject[]
 export type TLayoutPermission = TLayoutRouteObject['id'][]
