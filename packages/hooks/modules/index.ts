05aea9ea707f4fb5e85f70f76db90209389a053e
luyao <luyao@mw-robot.com>
2023-06-29 11:44:30 +0800

    feat: 添加useCrud

--- packages/hooks/modules/index.ts
+++ packages/hooks/modules/index.ts
@@ -1,3 +1,4 @@
 export { default as useStorage } from './useStorage'
 export { default as useRoute } from './useRoute'
 export { default as useOptions } from './useOptions'
+export { default as useCrud } from './useCrud'
