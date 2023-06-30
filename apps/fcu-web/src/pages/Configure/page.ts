2c1a90414b5ed891958ece48ca91a2945eac513b
luyao <luyao@mw-robot.com>
2023-06-30 09:09:38 +0800

    feat: 路由约定化配置

---
+++ apps/fcu-web/src/pages/Configure/page.ts
@@ -0,0 +1,4 @@
+export default {
+  name: '配置',
+  id: 'configure'
+}
--- apps/fcu-web/src/router/index.tsx
+++ apps/fcu-web/src/router/index.tsx
@@ -1,19 +1,64 @@
-import { lazy } from 'react'
+import { ComponentType, lazy } from 'react'
 import { Navigate } from 'react-router-dom'
-import { type TLayoutRoutes } from '@packages/types'
+import { ILayoutData, type TLayoutRoutes } from '@packages/types'
 
 const Layout = lazy(() => import('../components/layout'))
 const NotFound = lazy(() => import('@packages/ui/components/404/NotFound'))
-const Configure = lazy(() => import('../pages/configure'))
 
-const defineRoutes: TLayoutRoutes = [
-  {
-    path: 'configure',
-    element: <Configure />,
-    name: '配置',
-    id: 'configure'
-  }
-]
+/**
+ * @description: 自动生成后台Layout下的routes 要求页面定义在src/pages目录下, 每个页面都有个page.ts定义路由元信息.如果存在嵌套路由, 需要按照src/pages/** /c-pages/** /index.tsx的方式定义
+ * @return {*}
+ */
+const genaratePageRoutes = () => {
+  const pageModule = import.meta.glob('../pages/**/page.ts', {
+    eager: true,
+    import: 'default'
+  })
+  const asyncComponentModule = import.meta.glob<boolean, string, { default: ComponentType<any> }>(
+    '../pages/**/index.tsx'
+  )
+  const notPageNameStrings = ['..', '.', 'pages', 'page.ts', 'c-pages']
+
+  const pageModuleEntries = Object.entries(pageModule)
+  const routes: TLayoutRoutes = []
+  const pagesInfo = pageModuleEntries
+    .map(([pageModulePath, meta]) => {
+      const pageNameStrings = pageModulePath.split('/').filter((x) => !notPageNameStrings.includes(x))
+      return {
+        path: pageNameStrings,
+        pageModulePath,
+        meta: meta as ILayoutData
+      }
+    })
+    .sort((a, b) => a.path.length - b.path.length)
+  pagesInfo.forEach((pageInfo) => {
+    const parentRoute = routes.find((route) => route.path === pageInfo.path[0])
+    const Component = lazy(asyncComponentModule[pageInfo.pageModulePath.replace('page.ts', 'index.tsx')])
+    const route = {
+      path: pageInfo.path.at(-1),
+      element: <Component />,
+      ...pageInfo.meta,
+      children: []
+    }
+    if (parentRoute) {
+      parentRoute.children!.push(route)
+    } else {
+      routes.push(route)
+    }
+  })
+
+  return routes
+}
+const defineRoutes = genaratePageRoutes()
+
+// const defineRoutes: TLayoutRoutes = [
+//   {
+//     path: 'configure',
+//     element: <Configure />,
+//     name: '配置',
+//     id: 'configure'
+//   }
+// ]
 
 const routes: TLayoutRoutes = [
   {
