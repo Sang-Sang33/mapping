89a7ec65917bf07240b5f23355139ddf144e9d32
luyao <luyao@mw-robot.com>
2023-07-05 11:34:33 +0800

    fix: 部署路径嵌套资源访问404问题

--- apps/elsa-designer/src/components/dashboard/pages/elsa-studio-root/elsa-studio-root.tsx
+++ apps/elsa-designer/src/components/dashboard/pages/elsa-studio-root/elsa-studio-root.tsx
@@ -76,7 +76,7 @@ export class ElsaStudioRoot {
     const httpClientFactory: () => Promise<AxiosInstance> = () => createHttpClient(serverUrl)
 
     if (this.config) {
-      await fetch(`${document.location.origin}/${this.config}`)
+      await fetch(`${document.location.origin + document.location.pathname}/${this.config}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error('HTTP error ' + response.status)
