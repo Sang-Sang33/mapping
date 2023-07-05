0482222bb0490bd835404b336f825a42b9d984c2
luyao <luyao@mw-robot.com>
2023-07-05 09:43:37 +0800

    build: 打包支持子包随机组合

--- apps/fcu-web/vite.config.ts
+++ apps/fcu-web/vite.config.ts
@@ -2,25 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true
+export default defineConfig((mode) => {
+  const base = mode.mode === 'production' ? './' : '/'
+
+  return {
+    base,
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true
+        }
       }
-    }
-  },
-  server: {
-    proxy: {
-      '/api/fcu': {
-        target: 'http://dev.multiway-cloud.com:25002',
-        changeOrigin: true
-      },
-      '/api/mapping': {
-        target: 'http://dev.multiway-cloud.com:25008',
-        changeOrigin: true
+    },
+    server: {
+      proxy: {
+        '/api/fcu': {
+          target: 'http://dev.multiway-cloud.com:25002',
+          changeOrigin: true
+        },
+        '/api/mapping': {
+          target: 'http://dev.multiway-cloud.com:25008',
+          changeOrigin: true
+        }
       }
+    },
+    build: {
+      outDir: '../../dist/fcu-web'
     }
   }
 })
