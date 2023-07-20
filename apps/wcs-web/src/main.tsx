0bcb35581d64f4d126a19dcc95bfd543a08d465d
luyao <luyao@mw-robot.com>
2023-07-20 11:33:44 +0800

    feat:  抽离公共的multiway config

--- apps/wcs-web/src/main.tsx
+++ apps/wcs-web/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import '@/styles/index.css'
 import '@/styles/global.less'
 import 'antd/dist/antd.variable.min.css'
-import '@/multiway'
+// import '@/multiway'
+import '@packages/multiway-config'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
