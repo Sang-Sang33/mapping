f2b468d334e1425b2ca9d6171029ca4c539f294b
luyao <luyao@mw-robot.com>
2023-07-21 17:13:21 +0800

    build: change server url

--- apps/elsa-designer/src/assets/config/init.js
+++ apps/elsa-designer/src/assets/config/init.js
@@ -1,4 +1,4 @@
 ;(function () {
-  window.__SERVER_URL__ = 'http://dev.multiway-cloud.com:25001' // 开发环境
-  // window.__SERVER_URL__ = '/proxy' // 生产环境
+  // window.__SERVER_URL__ = 'http://dev.multiway-cloud.com:25001' // 开发环境
+  window.__SERVER_URL__ = '/proxy' // 生产环境
 })()
