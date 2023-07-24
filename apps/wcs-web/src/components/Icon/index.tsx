09288087d1208c11721ce22922d4f589fc9d0ccb
luyao <luyao@mw-robot.com>
2023-07-24 15:12:59 +0800

    fix: iconfont 404问题

--- apps/wcs-web/src/components/Icon/index.tsx
+++ apps/wcs-web/src/components/Icon/index.tsx
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons'
 
 // export const ICON_URL = "//at.alicdn.com/t/font_2827128_3t87jxevm8o.js";
-export const ICON_URL = '/iconfont.js'
+export const ICON_URL = import.meta.env.DEV ? '/iconfont.js' : '/wcs-web/iconfont.js'
 
 export const IconFont = createFromIconfontCN({
   scriptUrl: ICON_URL,
