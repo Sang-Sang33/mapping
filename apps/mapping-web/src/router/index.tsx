f8590080c810f17683a5104b4f4155bd32a75880
luyao <luyao@mw-robot.com>
2023-08-19 17:39:29 +0800

    fix: update redirect to sso function

--- apps/mapping-web/src/router/index.tsx
+++ apps/mapping-web/src/router/index.tsx
@@ -1,31 +1,28 @@
-import { Suspense } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import routes from "./routes";
-import lazyLoad from "@/router/utils/lazyLoad";
-import { redirectToSso, getAccessToken } from '@/utils/auth'
+import { Suspense } from 'react'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import routes from './routes'
+import lazyLoad from '@/router/utils/lazyLoad'
+import { getAccessToken } from '@/utils/auth'
+import { redirectToSSO } from '@packages/utils'
 function RouterConfig() {
-	if (!getAccessToken()) redirectToSso()
-	return (
-		<Router>
-			<Suspense>
-				<Routes>
-					{routes?.map(item => {
-						return (
-							<Route
-								path={item.path}
-								element={lazyLoad(item.component)}
-								key={item.name}
-							>
-								{item.children?.map(child => {
-									return <Route path={child.path} element={lazyLoad(child.component)} key={child.name} />;
-								})}
-							</Route>
-						);
-					})}
-				</Routes>
-			</Suspense>
-		</Router>
-	);
+  if (!getAccessToken()) redirectToSSO()
+  return (
+    <Router>
+      <Suspense>
+        <Routes>
+          {routes?.map((item) => {
+            return (
+              <Route path={item.path} element={lazyLoad(item.component)} key={item.name}>
+                {item.children?.map((child) => {
+                  return <Route path={child.path} element={lazyLoad(child.component)} key={child.name} />
+                })}
+              </Route>
+            )
+          })}
+        </Routes>
+      </Suspense>
+    </Router>
+  )
 }
 
-export default RouterConfig;
+export default RouterConfig
