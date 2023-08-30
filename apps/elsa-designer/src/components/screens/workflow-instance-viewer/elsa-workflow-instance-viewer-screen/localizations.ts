f3e7612984549c1617478282e2b81136f3b53c1b
luyao <luyao@mw-robot.com>
2023-08-30 11:00:40 +0800

    feat: workflow instance相关组件多语言

---
+++ apps/elsa-designer/src/components/screens/workflow-instance-viewer/elsa-workflow-instance-viewer-screen/localizations.ts
@@ -0,0 +1,38 @@
+export const resources = {
+  en_US: {
+    default: {
+      Stats: {
+        Event: 'Event',
+        Count: 'Count',
+        Empty: 'No events record for this activity'
+      }
+    }
+  },
+  zh_CN: {
+    default: {
+      Stats: {
+        Event: '事件',
+        Count: '数量',
+        Empty: '此活动没有事件记录'
+      }
+    }
+  },
+  ja_JP: {
+    default: {
+      Stats: {
+        Event: 'イベント',
+        Count: '数',
+        Empty: 'このアクティビティのイベント記録はありません'
+      }
+    }
+  },
+  ko_KR: {
+    default: {
+      Stats: {
+        Event: '이벤트',
+        Count: '개수',
+        Empty: '이 활동에 대한 이벤트 기록이 없습니다'
+      }
+    }
+  }
+}
