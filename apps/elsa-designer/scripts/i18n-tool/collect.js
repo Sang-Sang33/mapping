5cc65c6b32a297907fb7adf6f204552df8388f95
luyao <luyao@mw-robot.com>
2023-08-30 15:49:24 +0800

    feat: 增加elsa词条收集脚本

---
+++ apps/elsa-designer/scripts/i18n-tool/collect.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+const { FilePattern, RootFolderPath, DirPattern } = require('./common')
+const { parseTSJsonFromFile, flattenLanguageJSON } = require('./utils/json')
+const json2csv = require('json2csv').parse
+
+// 用于存储收集到的语言数据
+const collectedData = []
+
+// 递归查找符合规则的文件
+const findFilesInDirectory = (dirPath, pattern) => {
+  const files = fs.readdirSync(dirPath)
+  files.forEach((file) => {
+    const filePath = path.join(dirPath, file)
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+      findFilesInDirectory(filePath, pattern)
+    } else if (stat.isFile() && pattern.test(file) && DirPattern.some((d) => dirPath.includes(d))) {
+      const fileContent = fs.readFileSync(filePath, 'utf8') // 读取文件内容
+      const json = parseTSJsonFromFile(fileContent) // 解析json
+      const flattenedData = flattenLanguageJSON(json) // 扁平化
+      const flattenedDataWithPath = flattenedData.map((d) => ({ path: dirPath, ...d }))
+      collectedData.push(...flattenedDataWithPath)
+    }
+  })
+}
+
+// 开始递归查找
+findFilesInDirectory(RootFolderPath, FilePattern)
+
+// 将收集到的数据转换为CSV格式字符串
+const csvData = json2csv(collectedData, { fields: Object.keys(collectedData[0]) })
+
+// 将CSV数据写入文件
+fs.writeFileSync(path.join(__dirname, 'languageData.csv'), csvData, 'utf-8')
+
+console.log('Language data collected and saved to languageData.csv')
---
+++ apps/elsa-designer/scripts/i18n-tool/common.js
@@ -0,0 +1,21 @@
+// 匹配文件名的正则表达式
+const FilePattern = /localizations\.ts$/i
+const DirPattern = [
+  'elsa-activity-editor-modal',
+  'elsa-activity-picker-modal',
+  'elsa-version-history-panel',
+  'elsa-workflow-definition-editor-screen',
+  'elsa-workflow-properties-panel',
+  'elsa-workflow-publish-button',
+  'elsa-workflow-settings-modal',
+  'elsa-workflow-test-panel',
+  'elsa-workflow-instance-viewer-screen',
+  'elsa-confirm-dialog',
+  'elsa-designer-panel',
+  'elsa-modal-dialog'
+]
+
+// 遍历的根文件夹路径
+const RootFolderPath = './src/components'
+
+module.exports = { FilePattern, DirPattern, RootFolderPath }
