a82374aafdde66b7ee8254452ae658f917bf5e74
luyao <luyao@mw-robot.com>
2023-09-01 20:04:21 +0800

    feat: mapping支持网站标题和logo替换

---
+++ apps/mapping-web/public/shared/global.js
@@ -0,0 +1,17 @@
+window.__DOCUMENT_TITLE__ = '地图编辑器' // 系统标题
+window.__APP_LIST__ = [
+  {
+    label: 'WCS',
+    link: 'http://www.dev.multiway-cloud.com:25022/wcs-web/'
+  },
+  {
+    label: 'FCU',
+    link: 'http://www.dev.multiway-cloud.com:25022/fcu-web/'
+  },
+  {
+    label: 'Mapping',
+    link: 'http://www.dev.multiway-cloud.com:25022/mapping-web/'
+  }
+] // 跳转系统列表
+
+document.title = window.__DOCUMENT_TITLE__ // 修改网站标题
